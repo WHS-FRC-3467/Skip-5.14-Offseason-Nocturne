46aa0ffe1f16e5ffa6f30522bed2c570a4c7f2c4
Bryson Halsey <brysonh99@gmail.com>
2024-08-10 22:02:28 -0400

    Updated simplesub example

--- src/main/java/frc/robot/subsystems/SimpleSubsystem.java
+++ src/main/java/frc/robot/subsystems/SimpleSubsystem.java
@@ -6,8 +6,13 @@ package frc.robot.subsystems;
 
 import java.util.function.DoubleSupplier;
 
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -31,8 +36,13 @@ public class SimpleSubsystem extends SubsystemBase {
   @Setter
   private State state = State.OFF;
 
+  TalonFX m_motor = new TalonFX(Constants.ExampleCTREMotorConfig.ID_Motor);
+  private final DutyCycleOut m_percent = new DutyCycleOut(0);
+  private final NeutralOut m_brake = new NeutralOut();
+
   /** Creates a new SimpleSubsystem. */
   public SimpleSubsystem() {
+    m_motor.getConfigurator().apply(Constants.ExampleCTREMotorConfig.motorConfig());
 
   }
 
@@ -41,9 +51,9 @@ public class SimpleSubsystem extends SubsystemBase {
     // This method will be called once per scheduler run
 
     if (state == State.OFF) {
-      // m_intakeMotor.setControl(m_brake);
+      m_motor.setControl(m_brake);
     } else {
-      // m_intakeMotor.setControl(m_percent.withOutput(state.getStateOutput()));
+      m_motor.setControl(m_percent.withOutput(state.getStateOutput()));
     }
   }
 
