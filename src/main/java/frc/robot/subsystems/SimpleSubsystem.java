9d6474b7a443be4060adb64fc253164403baabd5
spybh66 <brysonh99@gmail.com>
2024-08-22 18:51:42 -0400

    Updated base examples

--- src/main/java/frc/robot/subsystems/SimpleSubsystem.java
+++ src/main/java/frc/robot/subsystems/SimpleSubsystem.java
@@ -8,6 +8,7 @@ import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.NeutralOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -30,6 +31,8 @@ public class SimpleSubsystem extends SubsystemBase {
   @Setter
   private State state = State.OFF;
 
+  private boolean debug = false;
+
   TalonFX m_motor = new TalonFX(Constants.ExampleSubsystemConstants.ID_Motor);
   private final DutyCycleOut m_percent = new DutyCycleOut(0);
   private final NeutralOut m_brake = new NeutralOut();
@@ -49,9 +52,21 @@ public class SimpleSubsystem extends SubsystemBase {
     } else {
       m_motor.setControl(m_percent.withOutput(state.getOutput()));
     }
+
+    displayInfo(debug);
   }
 
   public Command setStateCommand(State state) {
     return startEnd(() -> this.state = state, () -> this.state = State.OFF);
+  }
+
+  private void displayInfo(boolean debug) {
+    if (debug) {
+      SmartDashboard.putString("SimpleSubsystem State ", state.toString());
+      SmartDashboard.putNumber("SimpleSubsystem Setpoint ", state.getOutput());
+      SmartDashboard.putNumber("SimpleSubsystem Output ", m_motor.getMotorVoltage().getValueAsDouble());
+      SmartDashboard.putNumber("SimpleSubsystem Current Draw", m_motor.getSupplyCurrent().getValueAsDouble());
+    }
+
   }
 }
