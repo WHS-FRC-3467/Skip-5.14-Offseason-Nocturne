5a0e7c2a347bbcc63a59a75dc74301065dc9d39f
Noah-H3467 <nhaskell26@team3467.org>
2024-08-27 18:17:23 -0400

    copy Intake.java from Noah's-branch

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -33,4 +33,64 @@ public class Constants {
             return m_configuration;
         }
     }
+    public static class CanConstants {
+        /* PDH Power Ports
+        0. Front Left Drive
+        1. Front Left Steer
+        2. Left Shooter
+        3. Back Left Steer
+        4. Back Left Drive
+        5. Mini Power Module
+        6. Left Arm Motor
+        7. N/A
+        8. Intake
+        9. N/A
+        10. N/A
+        11. Right Arm
+        12. Candle
+        13. N/A
+        14. Back Right Steer
+        15. Back Right Drive
+        16. Front Right Drive
+        17. Front Right Steer
+        18. Right Shooter Motor
+        19. Stage Motor
+        20. Future Limelight
+        21. VRM
+        22. RIO
+        23. N/A
+        30. Blower Motor
+         * 
+         * 
+         */
+
+        // Drivebase CAN IDs are 1 -> 13
+        // See generated/TunerConstants.java
+        // Shooter CAN IDs
+        public static final int ID_ShooterLeft = 15;
+        public static final int ID_ShooterRight = 17;
+
+        // Intake CAN IDs
+        public static final int k_INTAKE_LEFT_CAN_ID = 19;
+        public static final int k_INTAKE_RIGHT_CAN_ID = 21;
+
+        //Stage CAN IDs
+        public static final int k_STAGE_CAN_ID = 23;
+
+        // Arm CAN IDs
+        public static final int ID_ArmLeader = 25;
+        public static final int ID_ArmFollower = 26;    
+        public static final int LED_CANDLE = 27;
+    }
+
+    public static class DIOConstants {
+        public static final int k_INTAKE_BEAM_BREAK = 1;
+        public static final int k_ARM_ENCODER_ID = 0;
+
+    }
+    
+    public static class IntakeConstants {
+        public static final double k_INTAKE_FWD_SPEED = -0.6;
+        public static final double k_INTAKE_REV_SPEED = 0.3;
+    }
 }
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,17 +1,125 @@
+
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+//import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+//import edu.wpi.first.wpilibj.DigitalInput;
+import frc.robot.Constants;
+import frc.robot.Constants.CanConstants;
+import frc.robot.Constants.DIOConstants;
+import frc.robot.Constants.IntakeConstants;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import java.util.function.DoubleSupplier;
 
 public class Intake extends SubsystemBase {
-  /** Creates a new Intake. */
-  public Intake() {}
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    /** Intake subsystem singleton. For superstructure. */
+    private static Intake instance = null;
+
+    @RequiredArgsConstructor
+    @Getter
+    public enum State{
+
+        FWD (() -> IntakeConstants.k_INTAKE_FWD_SPEED),
+        REV (() -> IntakeConstants.k_INTAKE_REV_SPEED),
+        OFF(() -> 0.0);
+
+        private final DoubleSupplier outputSupplier;
+
+        private double getStateOutput() {
+            return outputSupplier.getAsDouble();
+        }
+    }
+
+    @Getter
+    @Setter
+    private State state = State.OFF;
+
+    // Initalize Motors and Beam Break
+    TalonFX m_intakeLead = new TalonFX(CanConstants.k_INTAKE_LEFT_CAN_ID);
+    TalonFX m_intakeFollow = new TalonFX(CanConstants.k_INTAKE_RIGHT_CAN_ID);
+
+    // For superstructure
+    /**
+    * Returns the intake subsystem instance.
+    *
+    * @return the intake subsystem instance.
+    */
+    public static Intake getInstance() {
+        if (instance == null) {
+        instance = new Intake();
+        }
+
+        return instance;
+    }
+
+    /** Creates a new IntakeSubsystem. */
+    public Intake() {
+        
+        var talonFXConfigurator = m_intakeLead.getConfigurator();
+        var limitConfigs = new CurrentLimitsConfigs();
+        var outputConfigs = new MotorOutputConfigs();
+
+        // enable stator current limit
+        limitConfigs.StatorCurrentLimit = 120;
+        limitConfigs.StatorCurrentLimitEnable = true;
+
+        // Set brake as neutralmodevalue
+        outputConfigs.NeutralMode = NeutralModeValue.Brake;
+
+        talonFXConfigurator.apply(limitConfigs);
+        talonFXConfigurator.apply(outputConfigs);
+        
+        m_intakeFollow.getConfigurator().apply(limitConfigs);
+        m_intakeFollow.getConfigurator().apply(outputConfigs);
+        m_intakeFollow.setControl(new Follower(m_intakeLead.getDeviceID(), false));
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+        displayInfo(true);
+
+        m_intakeLead.set(state.getStateOutput());
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
+    }
+
+    /**
+     * Example command factory method. Periodic tells the intake to run according to the state
+     *
+     * @return a command setting the intake state to the argument
+     */
+    public Command setStateCommand(State intakestate) {
+        return startEnd(() -> this.state = intakestate, () -> this.state = State.OFF);
+    }
+
+    public void displayInfo(boolean debug) {
+        if (debug) {
+            SmartDashboard.putString("Intake State", getState().toString());
+            SmartDashboard.putNumber("Intake Setpoint", state.getStateOutput());
+            SmartDashboard.putNumber("Intake speed", m_intakeLead.getVelocity().getValueAsDouble());
+            SmartDashboard.putNumber("Intake Current Draw", m_intakeLead.getSupplyCurrent().getValueAsDouble());
+        }
+    }
 }
